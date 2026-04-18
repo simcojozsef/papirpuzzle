a2095d0e197fc93c1d30b476b71d8d481012e357
Simco József <simcojozsef@gmail.com>
2026-04-18 03:52:36 +0200

    fix api endpoint to fastapi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,8 @@ function App() {
       setProgress(0);
 
       const res = await axios.post(
-        "http://127.0.0.1:8000/api/upload",
+        //"http://127.0.0.1:8000/api/upload",
+        "https://papirpuzzle.onrender.com/reconstruct/",
         formData,
         {
           headers: {
